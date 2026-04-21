f3a0ba52a9e55f79a9fa544c77485134a4242007
lalguacp <lalguacp@emeal.nttdata.com>
2026-04-21 09:32:45 +0200

    Primer commit

---
+++ PokemonIA/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  PokemonIA
+//
+//  Created by Laura Alguacil Priego on 21/4/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ PokemonIA/PokemonIAApp.swift
@@ -0,0 +1,17 @@
+//
+//  PokemonIAApp.swift
+//  PokemonIA
+//
+//  Created by Laura Alguacil Priego on 21/4/26.
+//
+
+import SwiftUI
+
+@main
+struct PokemonIAApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
